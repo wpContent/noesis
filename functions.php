ad98a02aab360c1ef7b7d0ec397e63ade6228b21
Luke Watts <luke@luke-watts.com>
2014-04-23 23:33:49 +0100

    Adding filter to correct wp_title not displaying on homepage

--- functions.php
+++ functions.php
@@ -34,4 +34,39 @@
    *
    * Translations can be added to the /languages/ directory.
    */
-  load_theme_textdomain( 'noesis', get_template_directory() . '/languages' );
+load_theme_textdomain( 'noesis', get_template_directory() . '/languages' );
+
+/**
+ * Create a nicely formatted and more specific title element text for output
+ * in head of document, based on current view.
+ *
+ * @since Noesis 1.0
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string The filtered title.
+ */
+function noesis_wp_title( $title, $sep ) {
+  global $paged, $page;
+
+  if ( is_feed() ) {
+    return $title;
+  }
+
+  // Add the site name.
+  $title .= get_bloginfo( 'name', 'display' );
+
+  // Add the site description for the home/front page.
+  $site_description = get_bloginfo( 'description', 'display' );
+  if ( $site_description && ( is_home() || is_front_page() ) ) {
+    $title = "$title $sep $site_description";
+  }
+
+  // Add a page number if necessary.
+  if ( $paged >= 2 || $page >= 2 ) {
+    $title = "$title $sep " . sprintf( __( 'Page %s', 'noesis' ), max( $paged, $page ) );
+  }
+
+  return $title;
+}
+add_filter( 'wp_title', 'noesis_wp_title', 10, 2 );
