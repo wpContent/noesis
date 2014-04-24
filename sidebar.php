add431b0a4abea336c570a335de34cd0798cb0fe
Luke Watts <luke@luke-watts.com>
2014-04-24 12:44:19 +0100

    Tidies up php and output HTML in sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -5,9 +5,13 @@
          * @uses $index (mixed) (required) Sidebar name or id.
          * @return (boolean) True if the sidebar is in use, otherwise the function returns false
          */
-        if( is_active_sidebar( 'noesis-right-sidebar' ) ) : ?><!-- START OF sidebar.php -->
+        if( is_active_sidebar( 'noesis-right-sidebar' ) ) : ?>
+
+        <!-- START OF sidebar.php -->
         <aside>
           <?php dynamic_sidebar( 'noesis-right-sidebar' ); ?>
+        
         </aside>
         <!-- END OF sidebar.php -->
-        <?php endif; ?>
+        
+      <?php endif; ?>
